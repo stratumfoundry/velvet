ea83d7759996160ad1e050b0deecbf7dd714541f
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2025-01-05 18:54:07 +0100

    fix: remove riverpod and add method to KernelContract

--- packages/velvet_framework/lib/src/kernel/contracts/kernel_contract.dart
+++ packages/velvet_framework/lib/src/kernel/contracts/kernel_contract.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:velvet_framework/src/core/plugin/velvet_plugin.dart';
 import 'package:velvet_framework/src/kernel/exceptions/kernel_is_already_running_exception.dart';
+import 'package:velvet_framework/src/kernel/mixins/setup_container_composition_mixin.dart';
 import 'package:velvet_framework/src/kernel/widgets/kernel_error_widget.dart';
 import 'package:velvet_framework/src/kernel/widgets/kernel_loading_widget.dart';
+
+typedef WidgetWrapperCallback = Widget Function(
+  Widget child,
+);
 
 abstract class KernelContract {
   KernelErrorWidget? errorWidget;
@@ -10,8 +15,6 @@ abstract class KernelContract {
 
   /// Indicates if the kernel is running
   var _isRunning = false;
-
-  ProviderContainer? riverpodContainer;
 
   /// This key is used to get the context of the Navigator
   /// It will be used to show dialogs, snackbars.
@@ -40,15 +43,34 @@ abstract class KernelContract {
 
   // Set the initialization's error widget of the application
   /// If not set, the default error widget will be used
-  ///
-  /// IMPORTANT: The widget must return a MaterialApp at its root
-  void usingError(KernelErrorWidget errorWidget);
+  void overrideErrorWidget(KernelErrorWidget errorWidget);
 
   /// Set the initialization's loading widget of the application
   /// If not set, the default loading widget will be used
+  void overrideLoadingWidget(KernelLoadingWidget loadingWidget);
+
+  /// Register a callback that will be executed before the app startup
+  void addBeforeAppStartupCallback(BeforeAppStartupCallback callback);
+
+  /// Register a callback that will be executed when the app is booting
+  void addBootCallback(BootCallback callback);
+
+  /// Register a callback that will be executed when the app is registering
+  void addRegisterCallback(RegisterCallback callback);
+
+  /// Register a plugin
+  void addPlugin(VelvetPlugin plugin);
+
+  /// Registers a callback that wraps a widget
+  /// This callback will be applied to all widgets that are wrapped by the KernelAppWidget
   ///
-  /// IMPORTANT: The widget must return a MaterialApp at its root
-  void usingLoading(KernelLoadingWidget loadingWidget);
+  /// IMPORTANT: This callback will be applied to all widgets that are wrapped by the KernelAppWidget
+  /// and will be applied in reverse order for nesting
+  ///
+  /// ```dart
+  /// kernel.registerWidgetCallback((context, child) => child);
+  /// ```
+  void addWidgetCallback(WidgetWrapperCallback callback);
 
   /// Run the application
   ///
