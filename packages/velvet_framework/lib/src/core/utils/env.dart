beb82c39af1feb2b2dfd060f7290abc4d9abf96b
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2025-01-05 18:53:39 +0100

    fix: env utils cannot take values from environment

--- packages/velvet_framework/lib/src/core/utils/env.dart
+++ packages/velvet_framework/lib/src/core/utils/env.dart
@@ -1,16 +1,8 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Get environment variable.
 String? env(String key, [String? defaultValue]) {
-  if (kDebugMode) {
-    return dotenv.env[key] ?? defaultValue;
-  }
-
-  return String.fromEnvironment(
-    key,
-    defaultValue: defaultValue ?? '',
-  );
+  return dotenv.env[key] ?? defaultValue;
 }
 
 /// Get environment variable as string.
@@ -20,30 +12,18 @@ String? envString(String key, [String? defaultValue]) {
 
 /// Get environment variable as integer.
 int? envInt(String key, [int? defaultValue]) {
-  if (kDebugMode) {
-    return int.tryParse(dotenv.env[key] ?? '') ?? defaultValue;
-  }
-
-  return int.tryParse(String.fromEnvironment(key)) ?? defaultValue;
+  return int.tryParse(dotenv.env[key] ?? '') ?? defaultValue;
 }
 
 /// Get environment variable as double.
 double? envDouble(String key, [double? defaultValue]) {
-  if (kDebugMode) {
-    return double.tryParse(dotenv.env[key] ?? '') ?? defaultValue;
-  }
-
-  return double.tryParse(String.fromEnvironment(key)) ?? defaultValue;
+  return double.tryParse(dotenv.env[key] ?? '') ?? defaultValue;
 }
 
 /// Get environment variable as boolean.
 // ignore: boolean_prefixes
 bool? envBool(String key, [bool? defaultValue]) {
-  if (kDebugMode) {
-    final value = dotenv.env[key] ?? defaultValue.toString();
+  final value = dotenv.env[key] ?? defaultValue.toString();
 
-    return value == 'true' ? true : false;
-  }
-
-  return bool.hasEnvironment(key) ? bool.fromEnvironment(key) : defaultValue;
+  return value == 'true' ? true : false;
 }
