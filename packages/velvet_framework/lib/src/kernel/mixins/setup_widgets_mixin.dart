c7484069e0bf094223def643bc90721b56ad229c
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2025-01-19 17:26:51 +0100

    feat: add widgetCallbacks to wrap widget tree building before startup and rename error and loading override methods

--- packages/velvet_framework/lib/src/kernel/mixins/setup_widgets_mixin.dart
+++ packages/velvet_framework/lib/src/kernel/mixins/setup_widgets_mixin.dart
@@ -1,14 +1,17 @@
+import 'package:flutter/widgets.dart';
 import 'package:velvet_framework/src/kernel/contracts/kernel_contract.dart';
 import 'package:velvet_framework/src/kernel/widgets/kernel_error_widget.dart';
 import 'package:velvet_framework/src/kernel/widgets/kernel_loading_widget.dart';
 
 mixin SetupWidgetsMixin on KernelContract {
+  final List<WidgetWrapperCallback> _widgetCallbacks = [];
+
   // Set the initialization's error widget of the application
   /// If not set, the default error widget will be used
   ///
   /// NOTE: The widget must return a MaterialApp at its root
   @override
-  void usingError(KernelErrorWidget errorWidget) {
+  void overrideErrorWidget(KernelErrorWidget errorWidget) {
     throwIfRunning();
 
     this.errorWidget = errorWidget;
@@ -17,11 +20,37 @@ mixin SetupWidgetsMixin on KernelContract {
   /// Set the initialization's loading widget of the application
   /// If not set, the default loading widget will be used
   ///
-  /// NOTE: The widget must return a MaterialApp at its root
   @override
-  void usingLoading(KernelLoadingWidget loadingWidget) {
+  void overrideLoadingWidget(KernelLoadingWidget loadingWidget) {
     throwIfRunning();
 
     this.loadingWidget = loadingWidget;
+  }
+
+  /// Register a callback to be called when the widget is created
+  ///
+  /// The callback will receive the widget as a parameter and must return the
+  /// widget to be used
+  @override
+  void addWidgetCallback(WidgetWrapperCallback callback) {
+    _widgetCallbacks.add(callback);
+  }
+
+  /// Apply the widget callbacks to the widget
+  ///
+  /// The widget callbacks are applied in the order they were registered
+  ///
+  /// The widget callbacks are applied to the widget before the widget is
+  /// returned
+  // ignore: unused_element, avoid_returning_widgets
+  @protected
+  Widget applyWidgetCallbacks(Widget baseWidget) {
+    Widget currentWidget = baseWidget;
+
+    for (final callback in _widgetCallbacks) {
+      currentWidget = callback(currentWidget);
+    }
+
+    return currentWidget;
   }
 }
