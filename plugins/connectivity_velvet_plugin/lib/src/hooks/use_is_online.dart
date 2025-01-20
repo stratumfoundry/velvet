115bc691720c9c4a2c385810d253865d74e8376d
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2025-01-20 17:12:19 +0100

    build(deps): remove flutter_hooks

--- plugins/connectivity_velvet_plugin/lib/src/hooks/use_is_online.dart
+++ plugins/connectivity_velvet_plugin/lib/src/hooks/use_is_online.dart
@@ -1,5 +1,6 @@
 import 'package:connectivity_velvet_plugin/connectivity_velvet_plugin.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:velvet_framework/velvet_framework.dart';
 
 ValueNotifier<bool> useIsOnline() {
